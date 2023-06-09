0c341a91bd79d4cbe236e151ae5eed39e07c7caf
butcanudothis <akshayvarma.varma@gmail.com>
2023-06-09 06:04:18 +0530

    input prop setup and email input implemented

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -7,6 +7,9 @@ import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from './Modal'
+import Heading from "../Heading";
+import Input from "../inputs/input";
+
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -38,6 +41,24 @@ const RegisterModal = () => {
         setIsLoading(false);
       });
   };
+
+  const bodyContent = (
+    <div className="flex flex-col space-y-4">
+      <Heading
+      title="Welcome to Venue"
+      subtitle="Create an account to continue"
+      />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors = {errors}
+        required
+      />
+    </div>
+  );
+
   return (
     <Modal
         disabled={isLoading}
@@ -46,6 +67,7 @@ const RegisterModal = () => {
         actionLabel="Continue"
         onClose={registerModal.onClose}
         onSubmit={handleSubmit(onSubmit)}
+        body = {bodyContent}
         />
   );
 };
