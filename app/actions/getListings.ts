8084467af023414a74a3c615be243cb2aeecea72
butcanudothis <akshayvarma.varma@gmail.com>
2023-06-17 03:49:08 +0530

    Listing Head Component Implemented

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,14 +1,18 @@
-import prisma from '@/app/libs/prismadb'
+import prisma from "@/app/libs/prismadb";
 export default async function getListings() {
-    try{
-        const listings = await prisma.listing.findMany({
-            orderBy: {
-                createdAt: 'desc'
-            },
-        });
-        return listings;
-    }
-    catch(err: any){
-        throw new Error(err);
-    }
+  try {
+    const listings = await prisma.listing.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    const safeListings = listings.map((listing) => ({
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
+    }));
+    return safeListings;
+  } catch (err: any) {
+    throw new Error(err);
+  }
 }
